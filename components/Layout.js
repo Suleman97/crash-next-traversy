6838eb3f17d6afcce6d5e20494dcfd4938b049c1
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-07-12 18:04:23 +0500

    create Navbar and style it.

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,19 @@
 
+import Nav from './Nav'
 import styles from "../styles/Layout.module.css";
 
 const Layout = ({ children }) => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        {children}
-      </main>
+    <>
+      <Nav />
+      <div className={styles.container}>
+        <main className={styles.main}>
+          {/* <h1>hello</h1> */}
+          {children}
+        </main>
 
-    </div>
+      </div>
+    </>
   )
 }
 
