c6f1b21764cb5042b9b30f512d4fa5e0f134e9b8
Muhammad Suleman <54745700+Suleman97@users.noreply.github.com>
2021-07-12 17:54:58 +0500

    understanding the basics of next js

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+
+import Head from "next/head"
+import head from "next/head"
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+
+      <h2>This is about page</h2>
+    </div>
+  )
+}
+
+export default About
